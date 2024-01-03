8caba4f13e32897321d6903475c4cf872b70adc3
Zeyo Li <zeyo0510@gmail.com>
2024-01-03 21:57:20 +0800

    add Utils.js

--- player.js
+++ player.js
@@ -35,69 +35,6 @@ var settings=
 , screenshotFormat : "jpg"
 };
 
-class Utils
-{
-  static delay(a, b)
-  {
-    let c = 0;
-    return function(...d)
-    {
-      clearTimeout(c);
-      c = setTimeout(a.bind(this, ...d), b);
-    }
-  }
-
-  static showInfo(a)
-  {
-    info.textContent = a;
-    info.classList.remove("d-none");
-    Utils.delayHideInfo();
-  }
-
-  static hideInfo()
-  {
-    info.classList.add("d-none");
-  }
-
-  static showError(a)
-  {
-    errorInfo.textContent = a;
-    errorInfo.classList.remove("d-none");
-    Utils.delayHideError();
-  }
-
-  static hideError()
-  {
-    errorInfo.classList.add("d-none");
-  }
-
-  static formatTime(a)
-  {
-    var b = Math.round(a);
-        a = Math.floor(b / 60);
-    b %= 60;
-    return `${10>a?"0"+a:a}:${10>b?"0"+b:b}`
-  }
-
-  static screenshot()
-  {
-    if (!(2 > video.readyState))
-    {
-      Utils.canvas||(Utils.canvas=document.createElement("canvas"));
-      Utils.file||(Utils.file=document.createElement("a"));
-      var a = Utils.canvas,
-          b = Utils.file;
-      a.width  = video.videoWidth;
-      a.height = video.videoHeight;
-      a.getContext("2d").drawImage(video, 0, 0, a.width, a.height);
-      settings && "png" == settings.screenshotFormat
-      ? (b.download = `screenshot-${video.currentTime.toFixed(3)}.png`, b.href = a.toDataURL("image/png")     )
-      : (b.download = `screenshot-${video.currentTime.toFixed(3)}.jpg`, b.href = a.toDataURL("image/jpeg",.98));
-      b.click()
-    }
-  }
-}
-
 Utils.delayHideInfo  = Utils.delay(Utils.hideInfo, 1500);
 Utils.delayHideError = Utils.delay(Utils.hideError, 1500);
 
