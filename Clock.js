f75cab62c60e9e836ba6b61c246347945c84b944
Zeyo Li <zeyo0510@gmail.com>
2024-01-03 21:59:27 +0800

    update Clock.js

---
+++ Clock.js
@@ -0,0 +1,33 @@
+class Clock
+{
+  static init()
+  {
+    let a = {
+      hourCycle : settings.timeFormatHour12 ? "h12" : "h23"
+    , hour      : "2-digit"
+    , minute    : "2-digit"
+    };
+    settings.timeFormatSecond && (a.second = "2-digit");
+    Clock.ClockFormat = new Intl.DateTimeFormat("en-GB", a)
+  }
+
+  static toggleClockTime()
+  {
+    if (rtInfo.classList.contains("d-none"))
+    {
+      Clock.updateClockTime(),
+      rtInfo.classList.remove("d-none"),
+      Clock.clockTimer = setInterval(Clock.updateClockTime, 1000)
+    } else {
+      rtInfo.classList.add("d-none");
+      clearInterval(Clock.clockTimer);
+    }
+  }
+
+  static updateClockTime()
+  {
+    rtInfo.textContent = Clock.ClockFormat.format(new Date);
+  }
+}
+
+globalThis.Clock = Clock;
--- player.js
+++ player.js
@@ -38,37 +38,6 @@ var settings=
 Utils.delayHideInfo  = Utils.delay(Utils.hideInfo, 1500);
 Utils.delayHideError = Utils.delay(Utils.hideError, 1500);
 
-class Clock
-{
-  static init()
-  {
-    let a = {
-      hourCycle : settings.timeFormatHour12 ? "h12" : "h23"
-    , hour      : "2-digit"
-    , minute    : "2-digit"
-    };
-    settings.timeFormatSecond && (a.second = "2-digit");
-    Clock.ClockFormat = new Intl.DateTimeFormat("en-GB", a)
-  }
-
-  static toggleClockTime()
-  {
-    if (rtInfo.classList.contains("d-none"))
-    {
-      Clock.updateClockTime(),
-      rtInfo.classList.remove("d-none"),
-      Clock.clockTimer = setInterval(Clock.updateClockTime, 1000)
-    } else {
-      rtInfo.classList.add("d-none");
-      clearInterval(Clock.clockTimer);
-    }
-  }
-
-  static updateClockTime()
-  {
-    rtInfo.textContent = Clock.ClockFormat.format(new Date);
-  }
-}
 
 class Player
 {
